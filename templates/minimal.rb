4c464e2805d26143ec25c92027c7bc3e458c7d08
duvan <duvanfoz@hotmail.com>
2023-02-02 16:05:41 -0500

    fix: FactoryBot configuration for rails applications

--- templates/minimal.rb
+++ templates/minimal.rb
@@ -43,9 +43,6 @@ after_bundle do
 
     RSpec.configure do |config|
       config.include FactoryBot::Syntax::Methods
-      config.before(:suite) do
-        FactoryBot.find_definitions
-      end
     end
   RUBY
 
